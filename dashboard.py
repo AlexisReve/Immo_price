450783336814892294a52907f03a622061d8e621
AlexisReve <alexis.revelle@hotmail.fr>
2023-06-30 09:49:57 +0200

    Changes

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,12 @@
 import pandas as pd 
+import numpy as np
 import streamlit as st
 from streamlit_folium import st_folium
 from streamlit_folium import folium_static
 import folium
 import streamlit.components.v1 as components
 import plotly.express as px
+import pickle
 
 st.set_page_config(layout = 'wide')
 
@@ -60,19 +62,33 @@ def main():
         st.plotly_chart(fig, use_container_width=True)
     
     st.subheader("Prédisez votre prix :")
-
+    
+    text = '''Attention, les résultats délivrés par le modèle ne peuvent être représentatif de la réalité.
+            Ce modèle à été construit à partir des bases de données de valeurs foncières disponibles sur la
+            plateformeData.gouv (2014-2021), les données concernent uniquement les transactions immobilières et ne
+            sont donc pas forcément représentatif du parc immobilier de chaque commune. En particulier lorsque le 
+            prix au m2 par commune est calculé sur un faible nombre de transactions immobilières. Les bases Demandes de
+            valeurs foncières ne sont pas suffisantes pour le cas d'usage de prédiction de prix. Celles-ci doivent être
+            enrichies d'autres données telles que des indicateurs de qualité de vie (nombre de délits, accès aux soins) ou
+            des indicateurs d'activité (taux de chômage, nombre d'entreprise). Si un premier travail de fusion de plusieurs
+            bases de données (listes gares sncf, taux de chômage par commune) a été effectué par nos soins, nous restons
+            tributaires de la disponibilité et de la qualité des données. Nous encourageons donc l'ensemble des acteurs
+            publiques et privés à travailler en étroite collaboration afin de mettre à disposition de nouvelles données
+            au citoyen.'''
+    st.info(text)
+    st.info("")
     with st.form(key = 'my_form'):
         with st.container():
             col3, col4, col5, col6 = st.columns(4)
 
             with col3:
-                Input_Maisons = st.text_input("Nombre de maisons vendues")
+                Input_Maisons = st.number_input("Nombre de maisons vendues")
 
             with col4:
-                Input_appart = st.text_input("Nombre d'appartements vendues")
+                Input_appart = st.number_input("Nombre d'appartements vendues")
             
             with col5:
-                Input_surface = st.text_input("Surface Moyenne")
+                Input_surface = st.number_input("Surface Moyenne")
 
             with col6:
                 Input_gare = st.selectbox("Gare", data["TGV"].unique().tolist())
@@ -82,6 +98,24 @@ def main():
             with col8:
                 submitted = st.form_submit_button("Appliquer les filtres")
 
+    if submitted:
+        input = [Input_Maisons, Input_appart, Input_surface]
+
+        with open('modele_xgboost.pkl', 'rb') as file:
+            model = pickle.load(file)
+
+        input_pas_gare = 1 if Input_gare == "Pas de gare" else 0
+        input_tgv = 1 if Input_gare == "Gare TGV" else 0
+        input_gare_voyageur = 1 if Input_gare == "Gare voyageurs non TGV" else 0
+        new_data = input + [input_pas_gare, input_gare_voyageur, input_tgv]
+        new_data = np.reshape(new_data, (1,-1))
+        predictions = pd.DataFrame(model.predict_proba(new_data)).rename(columns = {0 : "[0:1107]",
+                                                                                    1 : "[1108 : 1451]",
+                                                                                    2 : "[1452 : 1922]",
+                                                                                    3: "[1922:]"})
+        st.table(predictions)
+                
         
+
 
 main()
--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-slim
 
-EXPOSE 8501
+EXPOSE 8080
 
 WORKDIR /app
 
@@ -10,4 +10,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-ENTRYPOINT ["streamlit", "run", "dashboard.py", "--server.port=8501", "--server.address=0.0.0.0"]
+CMD streamlit run --server.port 8080 --server.enableCORS false dashboard.py
